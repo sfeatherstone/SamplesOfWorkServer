76a304ef93317d7193ef783ec679243857109f5d
simonfea <simon@gsx-r750.co.uk>
2017-01-04 21:30:26 +0000

    Add project files.

---
+++ SamplesOfWorkServer/Controllers/CardsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.IO;
+using System.Web.Http.Routing;
+using System.Web.Http.Controllers;
+using System.Net.Http;
+
+namespace SamplesOfWorkServer.Controllers
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class card
+    {
+        [JsonProperty]
+        public String id;
+        [JsonProperty]
+        public String title;
+        [JsonProperty]
+        public String image_url;
+        public String internalImage_url;
+        [JsonProperty]
+        public String description;
+    }
+
+    public class Cards
+    {
+        public List<card> cards;
+    }
+
+    public class CardsController : ApiController
+    {
+        static private Cards cards = null;
+
+        static private void buildChats(UrlHelper Url)
+        {
+            if (cards == null)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "SamplesOfWorkServer.Controllers.cards.json";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    cards = JsonConvert.DeserializeObject<Cards>(result);
+                    foreach (var c in cards.cards)
+                    {
+                        if (c.internalImage_url != null)
+                        {
+                            c.image_url = Url.Content(c.internalImage_url);
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+        // GET api/cards
+        [SwaggerOperation("GetAll")]
+        public Cards Get()
+        {
+            buildChats(Url);
+            return CardsController.cards;
+        }
+    }
+}
---
+++ SamplesOfWorkServer/Controllers/ChatController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.IO;
+
+namespace SamplesOfWorkServer.Controllers
+{
+    public class chat
+    {
+        public String username;
+        public String content;
+        public String userImage_url;
+        public String time;
+    }
+
+    public class Conversation
+    {
+        public List<chat> chats;
+    }
+
+    public class ChatController : ApiController
+    {
+        static private Conversation chats = null;
+
+        static private void buildChats()
+        {
+            if (chats == null)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "SamplesOfWorkServer.Controllers.chat.json";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    chats = JsonConvert.DeserializeObject<Conversation>(result);
+                }
+                
+            }
+        }
+        
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public Conversation Get()
+        {
+            buildChats();
+            return chats;// new string[] { "value1", "value2" };
+        }
+    }
+}
